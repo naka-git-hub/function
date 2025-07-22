c6c4996c7d6b43af53681334ef2b62c6f238cda7
naka-git-hub <naka.rafilipomanana@gmail.com>
2025-07-22 11:41:50 +0200

    first-commit

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+//alert("Hello world");
+
+//une fonction firstFunction qui contient une alerte("Hello world");
+
+// function firstFunction() {
+//     alert("Hello world");
+// }
+
+// firstFunction();
+
+// function testFunction() {
+//     alert("Test fonction");
+// }
+
+// testFunction();
+
+// Créer une fonction firstFunction.
+// function firstFunction() {
+//     alert("First");
+//     secondFunction();
+// }
+
+// Créer une fonction secondFunction.
+// function secondFunction() {
+//     alert("Second");
+// }
+
+// Appeler la fonction firstFunction
+// firstFunction();
+
+// function first() {
+//     let toto = "toto";
+//     let tata = "tata";
+//     second(toto, tata);
+// }
+
+// function second(toto, tata) {
+//     console.log(toto, tata);
+// }
+
+// first();
+
+// exo 1
